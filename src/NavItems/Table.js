470c38eb52a13237851be92dd44e15fb62219859
cvinicioyupa <137367433+MarlonQuishpi@users.noreply.github.com>
2023-06-24 14:49:48 -0500

    tables

--- src/NavItems/Table.js
+++ src/NavItems/Table.js
@@ -3,9 +3,9 @@ import MyTable from "../MyTable";
 const Table = () => {
   return (
     <div>
-      <MyTable nColumns={100} nRows={65} />
+      <MyTable nColumns={50} nRows={65} />
       <MyTable nColumns={15} nRows={90} />
-      <MyTable nColumns={100} nRows={5} />
+      <MyTable nColumns={50} nRows={5} />
     </div>
   );
 };

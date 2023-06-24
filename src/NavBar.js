4c69bff3260e40b75e51a765fa53338a268e1c21
cvinicioyupa <137367433+MarlonQuishpi@users.noreply.github.com>
2023-06-24 14:40:41 -0500

    images

--- src/NavBar.js
+++ src/NavBar.js
@@ -11,7 +11,7 @@ function NavBar() {
       <nav className='navbar'>
         <div className='nav-container'>
           <NavLink exact to='/' className='nav-logo'>
-            Marlon | Mi Portafolio
+            Marlon | Ecuador
             <i className='fas fa-code'></i>
           </NavLink>
 
